01d55b9a19dabd740d91e81dbdc189bcf6cd914e
Saransh Chauhan <k.saranshchauhan@gmail.com>
2024-10-31 16:43:04 +0530

    created api's

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const app = express();
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000/', 
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'], 
   credentials: true 
 }));
